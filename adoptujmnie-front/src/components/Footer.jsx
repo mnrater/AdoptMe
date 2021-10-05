82117bf2aa9a04d6af19f035faa13e249ffbd146
mnrater <michalniski1@gmail.com>
2021-10-05 05:27:13 +0200

    Footer component edited

--- adoptujmnie-front/src/components/Footer.jsx
+++ adoptujmnie-front/src/components/Footer.jsx
@@ -3,39 +3,43 @@ import React, { useEffect, useState } from "react";
 export const Footer = () => {
 
     return (
-    <>
-    <div className="footer_container"> 
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"></link>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css"></link>
+        <>
+            <div className="footer_container">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"></link>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css"></link>
                 <div class="footer-dark">
                     <footer>
                         <div class="container">
                             <div class="row">
                                 <div class="col-sm-6 col-md-3 item">
-                                    <h3>Services</h3>
+                                    <h3>Funkcjonalności</h3>
                                     <ul>
-                                        <li><a href="#">Web design</a></li>
-                                        <li><a href="#">Development</a></li>
-                                        <li><a href="#">Hosting</a></li>
+                                        <li><a href="#">Adoptuj zwierzę</a></li>
+                                        <li><a href="#">Dofinansuj schronisko</a></li>
+                                        <li><a href="#">Blog</a></li>
+                                        <li><a href="#">Pomoc</a></li>
                                     </ul>
                                 </div>
                                 <div class="col-sm-6 col-md-3 item">
-                                    <h3>About</h3>
+                                    <h3>Informacje</h3>
                                     <ul>
-                                        <li><a href="#">Company</a></li>
-                                        <li><a href="#">Team</a></li>
-                                        <li><a href="#">Careers</a></li>
+                                        <li><a href="#">O nas</a></li>
+                                        <li><a href="#">Regulamin</a></li>
                                     </ul>
+                                </div>
+                                <div class="col-md-6 item text">
+                                    <h3>Adoptuj Mnie</h3>
+                                    <p>Strona stworzona z miłości do zwierząt. Tutaj masz możliwość dać szansę jednemu z tysięcy zwierząt, które żyją w schroniskach. Nie możesz kupić miłości, ale możesz ją adoptować!</p>
                                 </div>
                                 <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
                             </div>
-                            <p class="copyright">Company Name © 2018</p>
+                            <p class="copyright">Adoptuj Mnie © 2021</p>
                         </div>
                     </footer>
                 </div>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
-                </div>
-            </>
-            )
+            </div>
+        </>
+    )
 }
